5a627030b8604aa3c459dc96ff4f03c8f1317256
philip ritchey <pcr@tamu.edu>
2022-07-03 17:02:14 -0500

    moved approved include tests to top of file

--- tests/tests_code.cpp
+++ tests/tests_code.cpp
@@ -1,4 +1,31 @@
 /*
+@number: 0
+@name: approved includes for code.cpp
+@points: 0
+@type: approved_includes
+@target: code.cpp
+*/
+<test/>
+
+/*
+@number: 0
+@name: approved includes for code.h
+@points: 0
+@type: approved_includes
+@target: code.h
+*/
+<test/>
+
+/*
+@number: 0
+@name: approved includes for code_tests.cpp
+@points: 0
+@type: approved_includes
+@target: code_tests.cpp
+*/
+<test/>
+
+/*
 @number: 1
 @name: code compiles without errors or warnings
 @points: 10
@@ -47,34 +74,4 @@
 */
 <test>
     EXPECT_EQ(foo(867), 5309);
-</test>
-
-/*
-@number: 0
-@name: approved includes for code.cpp
-@points: 0
-@type: approved_includes
-@target: code.cpp
-*/
-<test>
-</test>
-
-/*
-@number: 0
-@name: approved includes for code.h
-@points: 0
-@type: approved_includes
-@target: code.h
-*/
-<test>
-</test>
-
-/*
-@number: 0
-@name: approved includes for code_tests.cpp
-@points: 0
-@type: approved_includes
-@target: code_tests.cpp
-*/
-<test>
 </test>
