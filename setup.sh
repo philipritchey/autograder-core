e9d08f3048dce887087da9983607e6415307c5fd
philip ritchey <pcr@tamu.edu>
2022-07-17 20:50:17 -0500

    install python3.8 if python3 is not at least 3.8

--- setup.sh
+++ setup.sh
@@ -7,10 +7,16 @@ apt-get -y upgrade
 # install dependencies
 #apt-get -y install apt-utils valgrind
 
-# python3.8
-#apt-get install software-properties-common
-#add-apt-repository -y ppa:deadsnakes/ppa
-#apt-get -y install python3.8
+# python version check (run_tests.py requires 3.8+)
+typeset -i python3_version=$(python3 --version | cut -d. -f2)
+if [ $python3_version -lt 8 ]; then
+  if [ "$(which python3.8)" == ""  ]; then
+    # install python3.8
+    apt-get install software-properties-common
+    add-apt-repository -y ppa:deadsnakes/ppa
+    apt-get -y install python3.8
+  fi
+fi
 
 cd /autograder/source
 mkdir -p /root/.ssh
@@ -21,6 +27,12 @@ cp ssh_config /root/.ssh/config
 #
 # Make sure to include your private key here (as `deploy_key`)
 #   this is the deploy key for the assignment-specific repo
+#   how-t0:
+#     $ ssh-keygen -t ed25519 -C "gradescope deploy key"
+#     save as ./deploy_key in assignment folder
+#     no passphrase
+#     add a new deploy key on github (Repo/Settings/Security/Deploy Keys)
+#       paste contents of deploy_key.pub
 mv deploy_key /root/.ssh/deploy_key
 chmod 600 /root/.ssh/deploy_key
 
