8834f381806a9a9ac7a0fe9c2dc553f12317f629
philip ritchey <pcr@tamu.edu>
2022-07-23 23:35:13 -0500

    put c++ memtest code in its own file instead of in script

--- tests/script_tests/code_memory_errors.sh
+++ tests/script_tests/code_memory_errors.sh
@@ -1,14 +1,12 @@
-# TODO(you): write memory test subject here
-{
-  echo "#include <iostream>"
-  echo "int main() {"
-  echo "   // use the function(s)"
-  echo "   return 0;"
-  echo "}"
-} > main.cpp
+# TODO(you): write memory error test in tests/memory_errors_test.cpp
+#            if you need several, you can use prefixes, e.g. codeA_memory_errors_test.cpp, codeB_memory_errors_test.cpp
+#              then have a version of this file for each
+#              TODO(pcr): abstract mem error test filename as an arg to the script 
+mv memory_errors_test.cpp main.cpp
 
-# TODO(you): replace $code with the name(s) of the source file(s) to use
-if g++ -std=c++17 -pedantic-errors -g $code.cpp main.cpp >> DEBUG 2>&1; then
+# TODO(you): specify the name(s) of the source file(s) to use
+CODE=( main.cpp )
+if g++ -std=c++17 -pedantic-errors -g "${CODE[@]}" >> DEBUG 2>&1; then
   if valgrind --leak-check=full --error-exitcode=1 ./a.out >> DEBUG 2>&1; then
 	  echo 100 > OUTPUT
 	  echo 'PASS: no memory errors detected' >> DEBUG
