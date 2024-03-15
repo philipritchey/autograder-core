526ec8e44ace00447e4da9ce2c7d17e8518859a3
Kathan Vyas <30324812+kathanvyas@users.noreply.github.com>
2024-03-15 12:36:29 -0500

    Adding Haskell Folder
    The commit includes creation of Haskell folder and adding unchanged files in the folder which are just place holders for now

---
+++ support/haskell/test_compiling.py
@@ -0,0 +1,67 @@
+from typing import List, Tuple
+from os import popen
+
+from attributes import Attributes
+from config import JAVA_CLASSPATH, JAVA_FLAGS, JAVAC
+from test_types import UnsupportedTestException
+
+
+def compile_x_test(src: List[str]) -> Tuple[bool,str]:
+    SRC = ' '.join(src)
+
+    compile_cmd = '{} {} {} 2>&1'.format(JAVAC, JAVA_FLAGS, SRC)
+    p = popen(compile_cmd)
+    try:
+        output = p.read()
+    except Exception as e:
+        output = str(e)
+    ret = p.close()
+    return ret == None, output
+
+def compile_unit_test(src: List[str]) -> Tuple[bool,str]:
+    return compile_x_test(src + ['UnitTest.java', 'UnitTestRunner.java', 'TestRunner.java'])
+
+def compile_performance_test(src: List[str]) -> Tuple[bool,str]:
+    return compile_x_test(src + ['PerformanceTest.java'])
+
+def compile_io_test(src: List[str]) -> Tuple[bool,str]:
+    return compile_x_test(src)
+
+def compile_script_test() -> Tuple[bool,str]:
+    return True, ""
+
+def compile_approved_includes_test() -> Tuple[bool,str]:
+    return True, ""
+
+def compile_coverage_test() -> Tuple[bool,str]:
+    return True, ""
+
+def compile_compile_test() -> Tuple[bool,str]:
+    return True, ""
+
+def compile_style_test() -> Tuple[bool,str]:
+    return True, ""
+
+def compile_test(test: Attributes) -> Tuple[bool, str]:
+    compiles = False
+    compile_output = ''
+    if test['type'] == 'unit':
+        compiles, compile_output = compile_unit_test([test['target'], test['include']])
+    elif test['type'] == 'i/o':
+        compiles, compile_output = compile_io_test([test['target'], test['include']])
+    elif test['type'] == 'script':
+        compiles, compile_output = compile_script_test()
+    elif test['type'] == 'performance':
+        compiles, compile_output = compile_performance_test([test['target'], test['include']])
+    elif test['type'] == 'approved_includes':
+        compiles, compile_output = compile_approved_includes_test()
+    elif test['type'] == 'coverage':
+        compiles, compile_output = compile_coverage_test()
+    elif test['type'] == 'compile':
+        compiles, compile_output = compile_compile_test()
+    elif test['type'] == 'style':
+        compiles, compile_output = compile_style_test()
+    else:
+        # don't try to compile an unsupported test
+        raise UnsupportedTestException(test['type'])
+    return compiles, compile_output
---
+++ support/haskell/test_writing.py
@@ -0,0 +1,79 @@
+from attributes import Attributes
+from test_types import UnsupportedTestException
+
+def write_unit_test(test: Attributes) -> None:
+    with open('UnitTest.java', 'wt') as f:
+        f.write('import static org.hamcrest.MatcherAssert.assertThat;\n')
+        f.write('import static org.hamcrest.Matchers.*;\n')
+        f.write('import static org.junit.Assert.assertThrows;\n\n')
+
+        f.write('import junit.framework.TestCase;\n')
+        f.write('import org.junit.Test;\n\n')
+
+        f.write('public class UnitTest extends TestCase {\n')
+        f.write('  @Test\n')
+        f.write('  public void testUnit() {\n')
+        f.write('    {}\n'.format('\n    '.join(test['code'].splitlines())))
+        f.write('  }\n')
+        f.write('}\n')
+
+def write_performance_test(test: Attributes) -> None:
+    with open('PerformanceTest.java', 'wt') as f:
+        f.write('public class PerformanceTest {\n')
+        f.write('  public static void main(String[] args) {\n')
+        f.write('    long start = System.currentTimeMillis();\n')
+        f.write('    {}\n'.format('\n    '.join(test['code'].splitlines())))
+        f.write('    long end = System.currentTimeMillis();\n')
+        f.write('    long milliseconds = end - start;\n')
+        f.write('    System.out.println("operation took " + milliseconds + " ms.");\n')
+        f.write('  }\n')
+        f.write('}\n')
+
+# Writes out the input and output strings
+def write_io_test(test: Attributes) -> None:
+    with open('input.txt', 'wt') as f:
+        f.write(test['expected_input'])
+        f.write("\n")
+    with open('output.txt', 'wt') as f:
+        f.write(test['expected_output'])
+
+def write_script_test(test: Attributes) -> None:
+    with open('script.sh', 'wt') as f:
+        f.write(test['script_content'])
+
+def write_approved_includes_test(test: Attributes) -> None:
+    test['script_content'] = f"./approved_includes.sh {test['target']} {' '.join(test['approved_includes'])}"
+    write_script_test(test)
+
+def write_coverage_test(test: Attributes) -> None:
+    test['script_content'] = f"./coverage.sh {test['target']} {test['include']} {' '.join(test['approved_includes'])}"
+    write_script_test(test)
+
+def write_compile_test(test: Attributes) -> None:
+    test['script_content'] = f"./compiles.sh {' '.join(test['approved_includes'])}"
+    write_script_test(test)
+
+def write_style_test(test: Attributes) -> None:
+    test['script_content'] = f"./check_style.sh {' '.join(test['approved_includes'])}"
+    write_script_test(test)
+
+def write_test(test: Attributes) -> None:
+    if test['type'] == 'unit':
+        write_unit_test(test)
+    elif test['type'] == 'i/o':
+        write_io_test(test)
+    elif test['type'] == 'script':
+        write_script_test(test)
+    elif test['type'] == 'approved_includes':
+        write_approved_includes_test(test)
+    elif test['type'] == 'performance':
+        write_performance_test(test)
+    elif test['type'] == 'coverage':
+        write_coverage_test(test)
+    elif test['type'] == 'compile':
+        write_compile_test(test)
+    elif test['type'] == 'style':
+        write_style_test(test)
+    else:
+        # don't try to write an unsupported test
+        raise UnsupportedTestException(test['type'])
